aaf39044255e3e195ab48edda462bae2b323d911
sea <229468038@qq.com>
2020-03-17 13:27:12 +0800

    8090

--- src/utils/global.js
+++ src/utils/global.js
@@ -7,7 +7,7 @@
 
 // 后台管理系统服务器地址
 //export const baseUrl = 'http://192.168.1.102:8088'
-export const baseUrl = 'http://localhost:8088'
+export const baseUrl = 'http://m30a183279.qicp.vip/'    // 内网穿透
 // 系统数据备份还原服务器地址
 export const backupBaseUrl = 'http://localhost:8002'
 
